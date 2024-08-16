9f7791691b951dbfc14841d0a341d12c22881fb3
asiphile <asiphilemthethethwa@gmail.com>
2024-08-16 14:17:48 +0200

    setting up project structure

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "setting up project structure"
+git branch -M main
+git remote add origin https://github.com/Asiphile1/ShoppingApp-CodeTribe.git
+git push -u origin main
